02fa9846e49c8031a43a2778e3027b1b988a79e1
Sandeep Rohilla <makaankhareedo.srohilla@gmail.com>
2021-07-25 01:36:58 +0530

    added cool background

--- src/App.js
+++ src/App.js
@@ -16,13 +16,19 @@ function App() {
   }
   return (
     <div className="App">
-      <div className="container p-5 mx-auto">
-        <div className="lg:w-3/6 mx-auto py-5">
-         <h2 className="text-xl font-semibold">What would you like type below and hit speaker button and listen...</h2>
-        </div>
-        <div className="bg-white shadow-md rounded-md lg:w-3/6 mx-auto p-5">
-            <Textarea handleChange={textareaValue} placeholder="Type here..."/>            
-            <TalkButton handleClick={handleClick} className={textarea ? 'text-gray-600 pointer-events-auto' : 'text-gray-400 pointer-events-none'} />
+      <div className="lg:w-3/6 p-5 mx-auto relative min-h-screen items-center flex">
+
+        <div className="mix-blend-multiply filter blur-xl opacity-30 absolute top-64 -left-24 w-4/6 h-4/6 rounded-full bg-purple-300"></div>
+        <div className="mix-blend-multiply filter blur-xl opacity-30 absolute bottom-64 -right-24 w-4/6 h-4/6 rounded-full bg-yellow-300"></div>
+
+        <div className="w-full mx-auto z-10">
+          <div className="mx-auto py-5">
+          <h2 className="text-xl md:text-2xl font-semibold">What would you like type below and hit speaker button and listen...</h2>
+          </div>
+          <div className="bg-white shadow-md rounded-md mx-auto p-5">
+              <Textarea handleChange={textareaValue} placeholder="Type here..."/>            
+              <TalkButton handleClick={handleClick} className={textarea ? 'text-gray-600 pointer-events-auto' : 'text-gray-400 pointer-events-none'} />
+          </div>
         </div>
       </div>
     </div>
